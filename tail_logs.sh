e1c93176205a4a7beb3f04da335504a7dd66e658
David <djryan012@users.noreply.github.com>
2023-12-04 02:49:51 -0800

    Update tail_logs.sh

--- tail_logs.sh
+++ tail_logs.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# Load configuration
-CONFIG_FILE="/usr/local/bin/config.yml"
-if [ ! -f "$CONFIG_FILE" ]; then
-  echo "Error: Config file not found!"
-  exit 1
-fi
+# Use jq to parse YAML file
+target_container=$(yq eval '.target_container' /app/config.yaml)
+mqtt_broker=$(yq eval '.mqtt_broker' /app/config.yaml)
+mqtt_topic=$(yq eval '.mqtt_topic' /app/config.yaml)
+mqtt_username=$(yq eval '.mqtt_username' /app/config.yaml)
+mqtt_password=$(yq eval '.mqtt_password' /app/config.yaml)
 
-eval "$(yaml2env -q "$CONFIG_FILE")"
-
-# Tail logs and publish to MQTT
+# Tail logs, filter specific lines, and publish to MQTT
 docker logs -f $target_container | while read -r line; do
-  mosquitto_pub -h $mqtt_broker -t $mqtt_topic -u $mqtt_username -P $mqtt_password -m "$line"
+  for "$keyword"; do
+    if echo "$line" | grep -q "$keyword"; then
+      mosquitto_pub -h $mqtt_broker -t $mqtt_topic -u $mqtt_username -P $mqtt_password -m "$line"
+      break
+    fi
+  done
 done
