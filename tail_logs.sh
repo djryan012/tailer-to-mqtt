6ae935cc5983e7ec8de182fb2e14b771cb5fffaa
David <djryan012@users.noreply.github.com>
2023-12-04 03:03:26 -0800

    Update tail_logs.sh

--- tail_logs.sh
+++ tail_logs.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Use jq to parse YAML file
-target_container=$(yq eval '.target_container' /app/config.yaml)
-mqtt_broker=$(yq eval '.mqtt_broker' /app/config.yaml)
-mqtt_topic=$(yq eval '.mqtt_topic' /app/config.yaml)
-mqtt_username=$(yq eval '.mqtt_username' /app/config.yaml)
-mqtt_password=$(yq eval '.mqtt_password' /app/config.yaml)
+target_container=$(yq eval '.target_container' /usr/local/bin/config.yml)
+mqtt_broker=$(yq eval '.mqtt_broker' /usr/local/bin/config.yml)
+mqtt_topic=$(yq eval '.mqtt_topic' /usr/local/bin/config.yml)
+mqtt_username=$(yq eval '.mqtt_username' /usr/local/bin/config.yml)
+mqtt_password=$(yq eval '.mqtt_password' /usr/local/bin/config.yml)
 
 # Tail logs, filter specific lines, and publish to MQTT
 docker logs -f $target_container | while read -r line; do
-  for "$keyword"; do
+  for keyword in "Version" "Starting Server" "Server Started"; do
     if echo "$line" | grep -q "$keyword"; then
       mosquitto_pub -h $mqtt_broker -t $mqtt_topic -u $mqtt_username -P $mqtt_password -m "$line"
       break
