6340e8306842cb4ff7c63d82ae7939e352ff1e99
David <djrconcepts@gmail.com>
2023-12-08 18:21:19 -0800

    check blank lines

--- log_reader.py
+++ log_reader.py
@@ -5,20 +5,25 @@ def monitor_logs(container_name, keywords):
     last_log_line = ""
 
     while True:
-        # Get the last log line from the specified container
-        command = f"docker logs --tail 1 {container_name}"
+        # Get all log lines from the specified container
+        command = f"docker logs {container_name}"
         try:
             output = subprocess.check_output(command, shell=True, text=True).strip()
 
-            # Check if the log line has changed
-            if output != last_log_line:
-                # Save the last log line
-                last_log_line = output
+            # Split the output into individual lines
+            log_lines = output.splitlines()
 
-                # Check if any keyword is present in the log line
-                if any(keyword in output for keyword in keywords):
-                    # If a match is found, print the line to the console
-                    print(output)
+            # Iterate through each line
+            for log_line in log_lines:
+                # Check if the log line is not blank and has changed
+                if log_line.strip() and log_line != last_log_line:
+                    # Save the last log line
+                    last_log_line = log_line
+
+                    # Check if any keyword is present in the log line
+                    if any(keyword in log_line for keyword in keywords):
+                        # If a match is found, print the line to the console
+                        print(log_line)
 
         except subprocess.CalledProcessError as e:
             print(f"Error while running 'docker logs': {e}")
