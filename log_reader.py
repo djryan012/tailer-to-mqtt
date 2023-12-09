cc42f00d401a5ddf17082c9a9deb50d82d88fe15
David <djrconcepts@gmail.com>
2023-12-08 18:53:21 -0800

    reprint

--- log_reader.py
+++ log_reader.py
@@ -3,15 +3,7 @@ import os
 from datetime import datetime
 
 def convert_to_datetime(log_timestamp):
-    formats = ["%Y-%m-%dT%H:%M:%S.%f", "%Y-%m-%d %H:%M:%S.%f"]
-    
-    for fmt in formats:
-        try:
-            return datetime.strptime(log_timestamp, fmt).strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]
-        except ValueError:
-            pass
-
-    raise ValueError(f"Could not parse timestamp: {log_timestamp}")
+    return datetime.strptime(log_timestamp[:-1], "%Y-%m-%dT%H:%M:%S.%f").strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]
 
 def monitor_logs(container_name, keywords):
     last_log_timestamp = ""
@@ -29,17 +21,14 @@ def monitor_logs(container_name, keywords):
             if log_lines:
                 last_log_timestamp = convert_to_datetime(log_lines[-1].split(' ', 1)[0])
 
+            # Iterate through each line
             for log_line in log_lines:
-                # Extract timestamp from the log line
-                log_timestamp = log_line.split(' ', 1)[0]
-
-                # Convert the timestamp
-                converted_timestamp = convert_to_datetime(log_timestamp)
-
-                # Check if any keyword is present in the log line
-                if any(keyword in log_line for keyword in keywords):
-                    # If a match is found, print the line to the console with the converted timestamp
-                    print(f"{converted_timestamp} {log_line}")
+                # Check if the log line is not blank
+                if log_line.strip():
+                    # Check if any keyword is present in the log line
+                    if any(keyword in log_line for keyword in keywords):
+                        # If a match is found, print the line to the console
+                        print(log_line)
 
         except subprocess.CalledProcessError as e:
             print(f"Error while running 'docker logs': {e}")
